1e8ad1327aff644bcaee0c83064a8875ec987b99
harshit27gupta <harshit.raj2023@gmail.com>
2025-06-11 08:11:16 +0530

    vercel analytics added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,20 @@
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
+import { AppProvider } from './Context/AppContext.jsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Analytics } from '@vercel/analytics/react';
 
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import { AppProvider } from './Context/AppContext.jsx'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')).render(
- <BrowserRouter>
- <AppProvider>
-    <QueryClientProvider client={queryClient}>
- <App/>
- </QueryClientProvider>
- </AppProvider>
- </BrowserRouter>
-)
+  <BrowserRouter>
+    <AppProvider>
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <Analytics />
+      </QueryClientProvider>
+    </AppProvider>
+  </BrowserRouter>
+);
