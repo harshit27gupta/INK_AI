f9862b302230c43fd79dc3dfd8197e140fd3fdd3
harshit27gupta <harshit.raj2023@gmail.com>
2025-06-12 08:20:51 +0530

    speed analytics added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { AppProvider } from './Context/AppContext.jsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Analytics } from '@vercel/analytics/react';
-
+import { SpeedInsights } from "@vercel/speed-insights/react"
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
@@ -14,6 +14,7 @@ createRoot(document.getElementById('root')).render(
       <QueryClientProvider client={queryClient}>
         <App />
         <Analytics />
+        <SpeedInsights />
       </QueryClientProvider>
     </AppProvider>
   </BrowserRouter>
